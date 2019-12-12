294b3e06b6d0697a438a36aef447d06f36fa7353
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-12 09:54:49 -0500

    initial commit

---
+++ earth-layers-realityKit/earth-layers-realityKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  earth-layers-realityKit
+//
+//  Created by Sunni Tang on 12/11/19.
+//  Copyright © 2019 Sunni Tang. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
